35de546d7658c7b29ba8e03708e734fbdfb93377
Ryan Atkinson <ryanatkn@gmail.com>
2021-01-27 01:34:03 -0700

    fancy hover state

--- src/EntityLink.svelte
+++ src/EntityLink.svelte
@@ -1,33 +1,66 @@
 <script>
 	import {vocabulary} from './vocabulary.js';
+	import {hoveredEntity} from './entities.js';
 
 	// one of these two is required
 	export let entity = null;
 	export let name = null;
 
+	window.linkCount = (window.linkCount || 0) + 1;
+	window.hack = hoveredEntity;
+
 	$: ent = name ? vocabulary.byName[name] : entity;
 
+	$: hovered = ent && ent === $hoveredEntity;
+
 	const colors = {
-		'vocab.core': '#dfe8f7',
-		'vocab.object': '#e6dff7',
-		'vocab.link': '#e6dff7',
-		'vocab.property': '#e3f4f7',
-		'vocab.activity': '#f2dff7',
-		'vocab.actor': '#f7dfe9',
+		'vocab.core': 218,
+		'vocab.object': 258,
+		'vocab.link': 258,
+		'vocab.property': 189,
+		'vocab.activity': 287,
+		'vocab.actor': 335,
 	};
 
-	$: color = colors[ent.category];
+	$: hue = colors[ent.category];
+	$: color = `hsl(${hue}, 60%, 93%)`;
+	$: hoveredColor = `hsl(${hue}, 60%, 87%)`;
+	$: pressedColor = `hsl(${hue}, 60%, 82%)`;
+
+	// TODO is there a significantly more efficient
+	// way to do this than listening to these events?
+	// there's 1270 instances of this component at last count lol
 </script>
 
-<a class="entity-link" href="#{ent.name}">
-	<slot entity={ent} {color}><code style="background-color: {color};"> {ent.name} </code></slot>
+<a
+	class="entity-link"
+	class:hovered
+	style="--color: {color}; --hovered-color: {hoveredColor}; --pressed-color: {pressedColor};"
+	href="#{ent.name}"
+	on:mouseenter={() => {
+		$hoveredEntity = ent;
+	}}
+	on:mouseleave={() => {
+		$hoveredEntity = null;
+	}}
+>
+	<slot entity={ent} {color}><code> {ent.name} </code></slot>
 </a>
 
 <style>
 	code {
+		background-color: var(--color);
 		padding: 0 3px;
 		margin: 3px;
 		border-radius: 3px;
 		line-height: 20px;
+	}
+	.hovered code {
+		background-color: var(--hovered-color);
+	}
+	.hovered code:active {
+		/* TODO store pressed state and style all `EntityLinks` with the pressed entity,
+		not just the sole active one */
+		background-color: var(--pressed-color);
 	}
 </style>
---
+++ src/entities.ts
@@ -0,0 +1,4 @@
+import {writable} from 'svelte/store';
+
+// TODO probably move to context
+export const hoveredEntity = writable(null);
