fffda4d52213103df77467c68443ea0a1ee2be4b
Ryan Atkinson <mail@ryanatkn.com>
2021-04-22 18:04:20 -0600

    test the deterministic id

--- src/markup.test.ts
+++ src/markup.test.ts
@@ -1,7 +1,7 @@
 import {suite} from 'uvu';
 import * as t from 'uvu/assert';
 
-import {toToId} from './markup.js';
+import {toToDeterministicId, toToId} from './markup.js';
 
 /* test_toToId */
 const test_toToId = suite('toToId');
@@ -23,3 +23,22 @@ test_toToId('basic behaior', () => {
 
 test_toToId.run();
 /* /test_toToId */
+
+/* test_toToDeterministicId */
+const test_toToDeterministicId = suite('toToDeterministicId');
+
+test_toToDeterministicId('basic behaior', () => {
+	const toIdA = toToDeterministicId();
+	const idA1 = toIdA();
+	const idA2 = toIdA();
+	const toIdB = toToDeterministicId();
+	const idB1 = toIdB();
+	const idB2 = toIdB();
+	t.is.not(idA1, idA2);
+	t.is.not(idB1, idB2);
+	t.is(idA1, idB1);
+	t.is(idA2, idB2);
+});
+
+test_toToDeterministicId.run();
+/* /test_toToDeterministicId */
