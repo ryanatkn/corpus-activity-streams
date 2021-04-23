7d12c42cf92dce17ad745033b6fea7f48a6a5377
Ryan Atkinson <mail@ryanatkn.com>
2021-04-22 22:00:28 -0600

    generate examples ast and add examples parse test

---
+++ src/activity_streams_examples_ast.gen.json.ts
@@ -0,0 +1,19 @@
+import type {Gen} from '@feltcoop/gro/dist/gen/gen.js';
+
+import {examples} from './activity_streams_examples.js';
+import {parseExamples} from './parse.js';
+
+// parses markup and outputs its AST - we're not using it yet, but it's cool
+
+export const gen: Gen = async () => {
+	return `{
+    ${Object.entries(examples).map(([name, examples]) => {
+			return `"${name}": ${JSON.stringify(parseExamples(examples, toId()))}`;
+		})}
+  }`;
+};
+
+let id = 0;
+const toId = () => {
+	return () => `node${id++}`;
+};
--- src/parse.test.ts
+++ src/parse.test.ts
@@ -1,7 +1,7 @@
 import {suite} from 'uvu';
 import * as t from 'uvu/assert';
 
-import {parseNotes} from './parse.js';
+import {parseExamples, parseNotes} from './parse.js';
 import {assignNodeIds, toToDeterministicId} from './markup.js';
 import type {MarkupNode} from './markup.js';
 
@@ -9,10 +9,6 @@ const normalize = (node: MarkupNode) => assignNodeIds(node, toToDeterministicId(
 
 /* test_parse */
 const test_parse = suite('parse');
-
-// test_parse('parseExamples', () => {
-// 	t.is(parseExamples('foo.task.ts'), 'foo');
-// });
 
 test_parse('parseNotes', () => {
 	t.equal(
@@ -23,6 +19,130 @@ test_parse('parseNotes', () => {
 				{type: 'Html', content: 'The ', id: 'node1'},
 				{type: 'Component', component: 'EntityLink', props: {name: 'Entity'}, id: 'node2'},
 				{type: 'Html', content: ' is the base of all of types.', id: 'node3'},
+			],
+			id: 'node0',
+		}),
+	);
+});
+
+test_parse('parseExamples', () => {
+	t.equal(
+		normalize(
+			parseExamples([
+				{
+					'@context': 'https://www.w3.org/ns/activitystreams',
+					type: 'Object',
+					id: 'http://www.test.example/object/1',
+					name: 'A Simple, non-specific object',
+				},
+			])!,
+		),
+		normalize({
+			type: 'Block',
+			children: [
+				{
+					type: 'Element',
+					element: 'pre',
+					children: [
+						{
+							type: 'Block',
+							children: [
+								{
+									type: 'Block',
+									children: [{type: 'Html', content: '{\n', id: 'node4'}],
+									id: 'node3',
+								},
+								{
+									type: 'Block',
+									children: [{type: 'Html', content: '\t@context: ', id: 'node6'}],
+									id: 'node5',
+								},
+								{
+									type: 'Block',
+									children: [
+										{
+											type: 'Html',
+											content: '"https://www.w3.org/ns/activitystreams",\n',
+											id: 'node8',
+										},
+									],
+									id: 'node7',
+								},
+								{
+									type: 'Block',
+									children: [
+										{type: 'Text', content: '\t', id: 'node10'},
+										{
+											type: 'Component',
+											component: 'EntityLink',
+											props: {name: 'type'},
+											id: 'node11',
+										},
+										{type: 'Text', content: ': ', id: 'node12'},
+									],
+									id: 'node9',
+								},
+								{
+									type: 'Block',
+									children: [{type: 'Html', content: '"Object",\n', id: 'node14'}],
+									id: 'node13',
+								},
+								{
+									type: 'Block',
+									children: [
+										{type: 'Text', content: '\t', id: 'node16'},
+										{
+											type: 'Component',
+											component: 'EntityLink',
+											props: {name: 'id'},
+											id: 'node17',
+										},
+										{type: 'Text', content: ': ', id: 'node18'},
+									],
+									id: 'node15',
+								},
+								{
+									type: 'Block',
+									children: [
+										{
+											type: 'Html',
+											content: '"http://www.test.example/object/1",\n',
+											id: 'node20',
+										},
+									],
+									id: 'node19',
+								},
+								{
+									type: 'Block',
+									children: [
+										{type: 'Text', content: '\t', id: 'node22'},
+										{
+											type: 'Component',
+											component: 'EntityLink',
+											props: {name: 'name'},
+											id: 'node23',
+										},
+										{type: 'Text', content: ': ', id: 'node24'},
+									],
+									id: 'node21',
+								},
+								{
+									type: 'Block',
+									children: [
+										{
+											type: 'Html',
+											content: '"A Simple, non-specific object"\n}',
+											id: 'node26',
+										},
+									],
+									id: 'node25',
+								},
+							],
+							id: 'node2',
+						},
+					],
+					id: 'node1',
+				},
 			],
 			id: 'node0',
 		}),
