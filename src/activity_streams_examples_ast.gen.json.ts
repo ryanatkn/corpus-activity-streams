b15e2fd888157f80a106cdfe3201ebf2d57fa4cc
Ryan Atkinson <mail@ryanatkn.com>
2021-04-25 17:59:03 -0600

    add a comment

--- src/activity_streams_examples_ast.gen.json.ts
+++ src/activity_streams_examples_ast.gen.json.ts
@@ -4,7 +4,8 @@ import {examples} from './activity_streams_examples.js';
 import {parseExamples} from './parseExamples.js';
 import {toToDeterministicId} from './tree.js';
 
-// parses markup and outputs its AST - we're not using it yet, but it's cool
+// parses markup and outputs its AST - we're not using it yet, but it's cool,
+// and combined with `gro check` acts as a big snapshot test
 
 export const gen: Gen = async () => {
 	const toId = toToDeterministicId();
