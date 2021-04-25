449b1cbce48ded088d0b439053d1b1bd861971fb
Ryan Atkinson <mail@ryanatkn.com>
2021-04-25 17:40:42 -0600

    refactor to use id helper

--- src/activity_streams_examples_ast.gen.json.ts
+++ src/activity_streams_examples_ast.gen.json.ts
@@ -2,18 +2,15 @@ import type {Gen} from '@feltcoop/gro/dist/gen/gen.js';
 
 import {examples} from './activity_streams_examples.js';
 import {parseExamples} from './parseExamples.js';
+import {toToDeterministicId} from './tree.js';
 
 // parses markup and outputs its AST - we're not using it yet, but it's cool
 
 export const gen: Gen = async () => {
+	const toId = toToDeterministicId();
 	return `{
     ${Object.entries(examples).map(([name, examples]) => {
-			return `"${name}": ${JSON.stringify(parseExamples(examples, toId()))}`;
+			return `"${name}": ${JSON.stringify(parseExamples(examples, toId))}`;
 		})}
   }`;
-};
-
-let id = 0;
-const toId = () => {
-	return () => `tree${id++}`;
 };
--- src/activity_streams_notes_ast.gen.json.ts
+++ src/activity_streams_notes_ast.gen.json.ts
@@ -2,18 +2,15 @@ import type {Gen} from '@feltcoop/gro/dist/gen/gen.js';
 
 import {vocabularyNotes} from './activity_streams_notes.js';
 import {parse} from './parse.js';
+import {toToDeterministicId} from './tree.js';
 
 // parses markup and outputs its AST - we're not using it yet, but it's cool
 
 export const gen: Gen = async () => {
+	const toId = toToDeterministicId();
 	return `{
     ${Object.entries(vocabularyNotes).map(([name, notes]) => {
-			return `"${name}": ${JSON.stringify(parse(notes, toId()))}`;
+			return `"${name}": ${JSON.stringify(parse(notes, toId))}`;
 		})}
   }`;
-};
-
-let id = 0;
-const toId = () => {
-	return () => `tree${id++}`;
 };
