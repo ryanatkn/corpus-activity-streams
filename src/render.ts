d3d7189913e080d8822d2ae8883a78050d88ff69
Ryan Atkinson <mail@ryanatkn.com>
2021-04-26 16:06:58 -0600

    refactor

---
+++ src/render.ts
@@ -0,0 +1,8 @@
+// Parsing JSON is a lot faster than parsing JS,
+// so we use this helper to improve the perf of some generated files.
+// TODO upstream helper
+export const toEmbeddedJson = (data: any): string => {
+	return JSON.stringify(data);
+	// const str = JSON.stringify(data).replace(/\'/g, "\\'");
+	// return `JSON.parse('${str}')`;
+};
