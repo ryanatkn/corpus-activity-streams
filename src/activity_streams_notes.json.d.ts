cbcfaedfcf0fc3d8a1242874da20d038657b2d78
Ryan Atkinson <mail@ryanatkn.com>
2021-07-18 12:52:42 -0500

    update tsconfig and simplify json types

--- src/activity_streams_notes.json.d.ts
+++ src/activity_streams_notes.json.d.ts
@@ -1,4 +1,4 @@
 declare module 'src/activity_streams_notes.json' {
-	const activity_streams_notes: {notes: Record<string, string>};
-	export default activity_streams_notes;
+	const data: {notes: Record<string, string>};
+	export default data;
 }
--- src/activity_streams_vocabulary.json.d.ts
+++ src/activity_streams_vocabulary.json.d.ts
@@ -1,5 +1,5 @@
 declare module 'src/activity_streams_vocabulary.json' {
 	import type {Vocabulary_Item} from 'src/activity_streams.js';
-	const activity_streams_vocabulary: {vocabulary: Vocabulary_Item[]};
-	export default activity_streams_vocabulary;
+	const data: {vocabulary: Vocabulary_Item[]};
+	export default data;
 }
