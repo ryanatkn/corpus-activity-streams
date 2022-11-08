2186e3519733ca3b537225bcab85301146132126
Ryan Atkinson <mail@ryanatkn.com>
2022-11-08 01:39:42 -0600

    fix nested object rendering in examples (#12)

--- src/lib/parse_examples.ts
+++ src/lib/parse_examples.ts
@@ -8,18 +8,7 @@ export const parse_examples = (examples: VocabularyTerm[]): Tree | null => {
 	const children: Tree[] = [];
 	for (const example of examples) {
 		const tree: BlockTree = {type: 'Block', children: []};
-		for (const [key, value] of Object.entries(example)) {
-			tree.children!.push({
-				type: 'Element',
-				element: 'div',
-				children: [
-					...parse('"' + key + '"'),
-					{type: 'Text', content: ': '},
-					...parse('"' + value + '"'),
-					{type: 'Text', content: ','},
-				],
-			});
-		}
+		parse_example(tree, example);
 		assign_node_ids(tree);
 		children.push({
 			type: 'Component',
@@ -28,4 +17,41 @@ export const parse_examples = (examples: VocabularyTerm[]): Tree | null => {
 		});
 	}
 	return assign_node_ids({type: 'Block', children});
+};
+
+const parse_example = (tree: Tree, value: any): void => {
+	const type = typeof value;
+	if (value === null) {
+		tree.children!.push({type: 'Text', content: 'null'});
+	} else if (Array.isArray(value)) {
+		tree.children!.push({type: 'Text', content: '['});
+		for (const item of value) {
+			const t: Tree = {
+				type: 'Element',
+				element: 'div',
+				children: [],
+			};
+			parse_example(t, item);
+			t.children!.push({type: 'Text', content: ','});
+			tree.children!.push(t);
+		}
+		tree.children!.push({type: 'Text', content: ']'});
+	} else if (type === 'object') {
+		tree.children!.push({type: 'Text', content: '{'});
+		for (const [k, v] of Object.entries(value)) {
+			const t: Tree = {
+				type: 'Element',
+				element: 'div',
+				children: [...parse('"' + k + '"'), {type: 'Text', content: ': '}],
+			};
+			parse_example(t, v);
+			t.children!.push({type: 'Text', content: ','});
+			tree.children!.push(t);
+		}
+		tree.children!.push({type: 'Text', content: '}'});
+	} else if (type === 'string') {
+		tree.children!.push(...parse('"' + value + '"'));
+	} else if (type === 'boolean' || type === 'number') {
+		tree.children!.push(...parse(value + ''));
+	}
 };
