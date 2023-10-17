b72e5de4ae31de6019be4ec09f519c61dd9a6a02
Ryan Atkinson <mail@ryanatkn.com>
2023-10-16 20:02:34 -0600

    wip

--- src/lib/parse.ts
+++ src/lib/parse.ts
@@ -173,7 +173,6 @@ const parseAttributes = (tagAttributes: string): {[key: string]: any} => {
 	let key = '';
 	let value = '';
 	let parsingAttributes: 'key' | 'value' = 'key'; // TODO const enum?
-	// eslint-disable-next-line @typescript-eslint/prefer-for-of
 	for (let i = 0; i < tagAttributes.length; i++) {
 		const char = tagAttributes[i];
 		if (parsingAttributes === 'key') {
